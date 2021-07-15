a9aef0a9e0785d94230f00d4ccc5b08e605abe49
JoaoSipauba <joaosipauba@hotmail.com>
2021-07-15 18:16:10 -0300

    feat: tipando listagem e mudando cor de acordo com a tipagem

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HighlightCard } from '../../components/HighlightCard';
-import { TransactionCard } from '../../components/TransactionCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 
 import { 
     Container, 
@@ -14,10 +14,51 @@ import {
     UserWrapper,
     HighlightCards,
     Transactions, 
-    Title
+    Title,
+    TransactionsList
 } from './styles';
 
+export interface DataListProps extends TransactionCardProps {
+    id: string;
+}
+
 export function Dashboard(){
+    const data: DataListProps[] = [
+        {   
+            id: '1',
+            type: 'positive',
+            title:"Desenvolvimento de site",
+            amount:"R$ 12.000,00",
+            date:"13/04/2020",
+            category:{
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            }
+        },
+        {
+            id: '2',
+            type: 'negative',
+            title:"Hamburgueria Pizzy",
+            amount:"R$ 59,00",
+            date:"13/04/2020",
+            category:{
+                name: 'Alimentação',
+                icon: 'coffee'
+            }
+        },
+        {
+            id: '3',
+            type: 'negative',
+            title:"Aluguel do apartamento",
+            amount:"R$ 1.200,00",
+            date:"13/04/2020",
+            category:{
+                name: 'Casa',
+                icon: 'shopping-bag'
+            }
+        },
+    ]
+    
     return(
         <Container>
             <Header>
@@ -57,7 +98,11 @@ export function Dashboard(){
             <Transactions>
                 <Title>Listagem</Title>
 
-                <TransactionCard />
+                <TransactionsList 
+                    data={data} 
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <TransactionCard data={item}/>}
+                />
             </Transactions>
         </Container>
     )
